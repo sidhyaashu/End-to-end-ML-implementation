131ba1901de74cfeedc14ef6ee057adbc00a37e8
sidhyaashu <ashutoshsidhya69@gmail.com>
2024-08-30 17:37:38 +0530

    model trainer added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from mlProject.logging import logger
 from mlProject.pipeline.step_01_data_ingestion_pipeline import DataIngestionTrainingPipeline
 from mlProject.pipeline.step_02_data_validation_pipeline import DataValidationTrainingPipeline
 from mlProject.pipeline.step_03_data_transformation_pipeline import DataTransformationTrainingPipeline
+from mlProject.pipeline.step_04_model_trainer_pipeline import ModelTrainerPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 if __name__ == '__main__':
@@ -33,6 +34,19 @@ if __name__ == '__main__':
     try:
         logger.info(f">>>>>>>> Stage {STAGE_NAME} Start <<<<<<<<<<")
         obj = DataTransformationTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> Stage {STAGE_NAME} Compelete <<<<<<<\n\nx==========x")
+
+    except Exception as e:
+        logger.exception(e)
+        raise e
+    
+
+STAGE_NAME = "Model trainer Statge"
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>>>> Stage {STAGE_NAME} Start <<<<<<<<<<")
+        obj = ModelTrainerPipeline()
         obj.main()
         logger.info(f">>>>>> Stage {STAGE_NAME} Compelete <<<<<<<\n\nx==========x")
 
---
+++ src/mlProject/pipeline/step_04_model_trainer_pipeline.py
@@ -0,0 +1,18 @@
+from mlProject.config.configuration import ConfigurationManager
+from mlProject.components.data_transformation import DataTransformation
+from mlProject import logger
+from pathlib import Path
+from mlProject.components.model_trainer import ModelTrainer
+
+STAGE_NAME = "Model trainer Statge"
+
+class ModelTrainerPipeline:
+    def __init__(self):
+        pass
+
+
+    def main(self):
+        config = ConfigurationManager()
+        model_trainer_config = config.get_model_trainer_config()
+        model_trainer_config = ModelTrainer(config=model_trainer_config)
+        model_trainer_config.train()
